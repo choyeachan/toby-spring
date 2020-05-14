684c1c4714264cc3acdcf81818de4769efa77527
조예찬 <choyeachan@gmail.com>
2020-05-14 15:05:50 +0900

    초기화 및 1권 1.1 초남감 DAO를 만들어본다 (~p.60)

---
+++ 01/src/springbook/Run.java
@@ -0,0 +1,30 @@
+package springbook;
+
+import java.sql.SQLException;
+
+import springbook.user.dao.UserDao;
+import springbook.user.domain.User;
+
+public class Run {
+
+	public static void main(String[] args) throws ClassNotFoundException,SQLException {
+
+		UserDao dao = new UserDao();
+
+		User user = new User();
+		user.setId("cyc");
+		user.setName("조예찬");
+		user.setPassword("depth1004");
+
+		dao.add(user);
+
+		System.out.println(user.getId() + " 등록성공");
+
+		User user2 = dao.get(user.getId());
+		System.out.println(user2.getName());
+		System.out.println(user2.getPassword());
+		System.out.println(user2.getId() + " 조회성공");
+
+	}
+
+}
