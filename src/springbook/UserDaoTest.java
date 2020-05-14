dd0c8f45604e0ee95f9e82b6df563194eaacb2c2
조예찬 <choyeachan@gmail.com>
2020-05-14 17:17:27 +0900

    ConnectionMaker를 구현한 클래스를 UserDao 를 사용할 클라이언트에서 주입하게 변경
    - 사용할 ConnectionMaker 타입의  오브젝트를 UserDao 가 아닌 UserDao 를 사용하는 Run 클래스에서 제공하므로서 UserDao의 필요없는 관심사항과 책임을 모두 UserDaoTest(클라이언트)  로 넘겨버렸다

--- src/springbook/UserDaoTest.java
+++ src/springbook/UserDaoTest.java
@@ -7,9 +7,10 @@ import springbook.user.dao.DConnectionMaker;
 import springbook.user.dao.UserDao;
 import springbook.user.domain.User;
 
-public class Run {
+public class UserDaoTest {
 
 	public static void main(String[] args) throws ClassNotFoundException,SQLException {
+
 		ConnectionMaker connectionMaker = new DConnectionMaker();
 		UserDao dao = new UserDao(connectionMaker);
 
