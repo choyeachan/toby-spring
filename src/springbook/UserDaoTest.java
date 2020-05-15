487bd9f55fc04fd6f7aeba57a5ec4833111959af
조예찬 <choyeachan@gmail.com>
2020-05-15 09:16:05 +0900

    UserDaoTest 클래스 관심사 분리
    - 어떻게 만들지와 어떻게 사용할지는 분명 다른 관심사다.
    - 테스트를 수행하는 UserDaoTest 는 어떠한 ConnectionMaker 을 사용할지는 관심사가 아니다.
    - Factory 클래스를 추가하여 해당 클래스를 이용하여 오브젝트를 받아오도록 변경한다.

--- src/springbook/UserDaoTest.java
+++ src/springbook/UserDaoTest.java
@@ -4,6 +4,7 @@ import java.sql.SQLException;
 
 import springbook.user.dao.ConnectionMaker;
 import springbook.user.dao.DConnectionMaker;
+import springbook.user.dao.DaoFactory;
 import springbook.user.dao.UserDao;
 import springbook.user.domain.User;
 
@@ -11,8 +12,7 @@ public class UserDaoTest {
 
 	public static void main(String[] args) throws ClassNotFoundException,SQLException {
 
-		ConnectionMaker connectionMaker = new DConnectionMaker();
-		UserDao dao = new UserDao(connectionMaker);
+		UserDao dao = new DaoFactory().userDao();
 
 		User user = new User();
 		user.setId("cyc");
